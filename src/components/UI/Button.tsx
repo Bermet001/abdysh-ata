12739f99b81ed51f9da9f4be0b6c84ddd24b63d9
Bermet Sheralieva <bermetsheralieva@MacBook-Pro-Bermet.local>
2024-12-18 18:21:07 +0600

    save

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,14 +1,46 @@
-import React from 'react'
-import { ReactNode } from 'react'
-import { Button as AntdButton } from 'antd'
+import { Button as AntdButton, ButtonProps } from 'antd'
+import { FC, forwardRef, ReactNode } from 'react'
+import { LoadingOutlined } from '@ant-design/icons'
 
-interface IProps {
-   onClick?: () => void
-   children: ReactNode
+interface IProps extends ButtonProps {
+   disabled: boolean
+   onClick: () => void
+   children?: ReactNode
+   variant?: 'primary' | 'secondary' | 'default' 
+   type?: 'button' | 'submit' | 'reset' | undefined 
+   colorLoading?: string
+   isLoading?: boolean
 }
 
-const Button: React.FC<IProps> = ({ onClick, children }) => {
-   return <AntdButton onClick={onClick}>{children}</AntdButton>
-}
+const Button: FC<IProps> = forwardRef<HTMLButtonElement, IProps>(
+   (
+      {
+         disabled,
+         onClick,
+         children,
+         variant,
+         type = 'submit',
+         colorLoading,
+         isLoading,
+         ...rest
+      },
+      ref
+   ) => (
+      <AntdButton
+         disabled={disabled}
+         onClick={onClick}
+         variant={variant}
+         type={isLoading ? 'button' : type}
+         ref={ref}
+         {...rest}
+      >
+         {isLoading ? (
+            <LoadingOutlined style={{ color: colorLoading }} />
+         ) : (
+            <>{children}</>
+         )}
+      </AntdButton>
+   )
+)
 
 export default Button
