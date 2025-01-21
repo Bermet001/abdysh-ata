1c3f4d2f05729d859c40f1aa0f0cdc8959f103c6
Bermet Sheralieva <bermetsheralieva@MacBook-Pro-Bermet.local>
2025-01-21 15:18:10 +0600

    added lazy loading

--- src/routes/UserRoutes.tsx
+++ src/routes/UserRoutes.tsx
@@ -1,24 +1,31 @@
 import { lazy } from 'react'
-import Suspense from './Suspense'
-import BannerInner from '../pages/user/1_inner-pages/BannerInner'
-import SchedulMatche from '../pages/user/matches/Matches'
-import Player from '../pages/user/team/Player'
-import New from '../pages/user/1_inner-pages/New'
-import NewsPage from '../pages/user/NewsPage'
-import Coaches from '../pages/user/coach/Сoaches'
-import Coach from '../pages/user/coach/Coach'
-import TeamWrapper from '../pages/user/team/TeamWrapper'
-import Partners from '../pages/user/Partners'
-import Contacts from '../pages/user/Contacts'
-import History from '../pages/user/History'
-import Infrastructures from '../pages/user/Infrastructure/Infrastructures'
-import Trophy from '../pages/user/trophy/Trophy'
-import Infrastracture from '../pages/user/Infrastructure/Infrastracture'
-import Rating from '../pages/user/rating-table/Rating'
-import Shop from '../pages/user/shop/Shop'
-import Gallery from '../pages/user/gallery/Gallery'
-import Photo from '../pages/user/gallery/Photo'
 const Landing = lazy(() => import('../pages/Landing'))
+const Photo = lazy(() => import('../pages/user/gallery/Photo'))
+const New = lazy(() => import('../pages/user/1_inner-pages/New'))
+const Gallery = lazy(() => import('../pages/user/gallery/Gallery'))
+const Shop = lazy(() => import('../pages/user/shop/Shop'))
+const Rating = lazy(() => import('../pages/user/rating-table/Rating'))
+const Infrastracture = lazy(
+   () => import('../pages/user/Infrastructure/Infrastracture')
+)
+const History = lazy(() => import('../pages/user/History'))
+const Contacts = lazy(() => import('../pages/user/Contacts'))
+const Partners = lazy(() => import('../pages/user/Partners'))
+const TeamWrapper = lazy(() => import('../pages/user/team/TeamWrapper'))
+const Coach = lazy(() => import('../pages/user/coach/Coach'))
+const Coaches = lazy(() => import('../pages/user/coach/Сoaches'))
+const NewsPage = lazy(() => import('../pages/user/NewsPage'))
+const Player = lazy(() => import('../pages/user/team/Player'))
+const SchedulMatche = lazy(() => import('../pages/user/matches/Matches'))
+const BannerInner = lazy(
+   () => import('../pages/user/1_inner-pages/BannerInner')
+)
+const Trophy = lazy(() => import('../pages/user/trophy/Trophy'))
+const Infrastructures = lazy(
+   () => import('../pages/user/Infrastructure/Infrastructures')
+)
+
+import Suspense from './Suspense'
 
 export const USER_ROUTES = [
    {
@@ -32,77 +39,149 @@ export const USER_ROUTES = [
 
    {
       path: 'match',
-      element: <SchedulMatche />,
+      element: (
+         <Suspense>
+            <SchedulMatche />,
+         </Suspense>
+      ),
    },
    {
       path: 'banner/:id',
-      element: <BannerInner />,
+      element: (
+         <Suspense>
+            <BannerInner />,
+         </Suspense>
+      ),
    },
    {
       path: 'player/:id',
-      element: <Player />,
+      element: (
+         <Suspense>
+            <Player />,
+         </Suspense>
+      ),
    },
    {
       path: 'news/:id',
-      element: <New />,
+      element: (
+         <Suspense>
+            <New />,
+         </Suspense>
+      ),
    },
    {
       path: 'news',
-      element: <NewsPage />,
+      element: (
+         <Suspense>
+            <NewsPage />,
+         </Suspense>
+      ),
    },
    {
       path: 'coaches',
-      element: <Coaches />,
+      element: (
+         <Suspense>
+            <Coaches />,
+         </Suspense>
+      ),
    },
    {
       path: 'team',
-      element: <TeamWrapper />,
+      element: (
+         <Suspense>
+            <TeamWrapper />,
+         </Suspense>
+      ),
    },
    {
       path: 'coaches/:id',
-      element: <Coach />,
+      element: (
+         <Suspense>
+            <Coach />,
+         </Suspense>
+      ),
    },
    {
       path: 'partners',
-      element: <Partners />,
+      element: (
+         <Suspense>
+            <Partners />,
+         </Suspense>
+      ),
    },
    {
       path: 'contacts',
-      element: <Contacts />,
+      element: (
+         <Suspense>
+            <Contacts />,
+         </Suspense>
+      ),
    },
    {
       path: 'history',
-      element: <History />,
+      element: (
+         <Suspense>
+            <History />,
+         </Suspense>
+      ),
    },
    {
       path: 'infrastracture',
-      element: <Infrastructures />,
+      element: (
+         <Suspense>
+            <Infrastructures />,
+         </Suspense>
+      ),
    },
    {
       path: 'infrastructure/:id',
-      element: <Infrastracture />,
+      element: (
+         <Suspense>
+            <Infrastracture />,
+         </Suspense>
+      ),
    },
    {
       path: 'trophy',
-      element: <Trophy />,
+      element: (
+         <Suspense>
+            <Trophy />,
+         </Suspense>
+      ),
    },
 
    {
       path: 'rating',
-      element: <Rating />,
+      element: (
+         <Suspense>
+            <Rating />,
+         </Suspense>
+      ),
    },
    {
       path: 'shop',
-      element: <Shop />,
+      element: (
+         <Suspense>
+            <Shop />,
+         </Suspense>
+      ),
    },
 
    {
       path: 'gallery',
-      element: <Gallery />,
+      element: (
+         <Suspense>
+            <Gallery />,
+         </Suspense>
+      ),
    },
 
    {
       path: 'gallery/:id',
-      element: <Photo />,
+      element: (
+         <Suspense>
+            <Photo />,
+         </Suspense>
+      ),
    },
 ]
