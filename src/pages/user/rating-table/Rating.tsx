3fbc65d6732e5d79ddac809b910994261d815f7c
Bermet Sheralieva <bermetsheralieva@MacBook-Pro-Bermet.local>
2025-03-27 16:22:35 +0600

    rating table

--- src/pages/user/rating-table/Rating.tsx
+++ src/pages/user/rating-table/Rating.tsx
@@ -28,40 +28,54 @@ const Rating = () => {
          dataIndex: 'index',
          key: 'index',
          align: 'center',
-         render: (text: string, record: TeamData, index: number) => index + 1,
+         render: (text: string, record: TeamData, index: number) => (
+            <h3
+               className="text-content"
+               style={{
+                  borderRadius: '6px',
+                  padding: '10px 20px',
+                  marginRight: '10px',
+               }}
+            >
+               {index + 1}
+            </h3>
+         ),
       },
 
       {
          title: '',
          dataIndex: 'team_title',
          key: 'team_title',
-         render: (text: string, record: TeamData) => (
-            <Flex
-               className="first-block-box"
-               style={{ width: '260px', borderRadius: '10px' }}
-               align="center"
-            >
-               <div
-                  style={{
-                     background: '#217c3d',
-                     padding: '10px',
-                     borderRadius: '10px',
-                  }}
-               >
+         render: (record: TeamData) => (
+            <Flex align="center">
+               <div style={{ background: 'transtapern', marginRight: '15px' }}>
                   <img
                      src={record.team_logo}
                      alt={`${record.team_title} logo`}
                      style={{
-                        width: '30px',
-                        height: '35px',
-                        borderRadius: '10px',
+                        padding: '5px',
+                        width: '45px',
+                        height: '50px',
                      }}
                   />
                </div>
-               <p style={{ borderRadius: '10px', background: '#217c3d' }}>
-                  {text}
-               </p>
             </Flex>
+         ),
+      },
+      {
+         title: '',
+         dataIndex: 'team_title',
+         key: 'team_title',
+         render: (text: string) => (
+            <h3
+               style={{
+                  width: '260px',
+                  borderRadius: '6px 0 0 6px',
+               }}
+               className="text-content"
+            >
+               {text}
+            </h3>
          ),
       },
       {
@@ -69,48 +83,65 @@ const Rating = () => {
          dataIndex: 'played',
          key: 'played',
          align: 'center',
+         render: (text: string) => <h3 className="text-content">{text}</h3>,
       },
       {
          title: 'В',
          dataIndex: 'won',
          key: 'won',
          align: 'center',
+         render: (text: string) => <h3 className="text-content">{text}</h3>,
       },
       {
          title: 'Н',
          dataIndex: 'drawn',
          key: 'drawn',
          align: 'center',
+         render: (text: string) => <h3 className="text-content">{text}</h3>,
       },
       {
          title: 'П',
          dataIndex: 'lost',
          key: 'lost',
          align: 'center',
+         render: (text: string) => <h3 className="text-content">{text}</h3>,
       },
       {
          title: 'З-П',
          dataIndex: 'goals_for',
          key: 'goals_for',
          align: 'center',
+         render: (text: string) => <h3 className="text-content">{text}</h3>,
       },
       {
          title: 'ПП',
          dataIndex: 'goals_against',
          key: 'goals_against',
          align: 'center',
+         render: (text: string) => <h3 className="text-content">{text}</h3>,
       },
       {
          title: '+/-',
          dataIndex: 'goal_difference',
          key: 'goal_difference',
          align: 'center',
+         render: (text: string) => <h3 className="text-content">{text}</h3>,
       },
       {
          title: '0',
          dataIndex: 'points',
          key: 'points',
          align: 'center',
+         render: (text: string) => (
+            <h3
+               className="text-content"
+               style={{
+                  borderRadius: '0 6px 6px 0',
+               }}
+            >
+               {text}
+            </h3>
+         ),
       },
    ]
 
@@ -135,8 +166,6 @@ const Rating = () => {
       <StyledContainer>
          <Flex vertical className="table">
             <div className="table-container">
-               <h1>Таблица рейтинга</h1>
-
                <Table
                   dataSource={dataSource}
                   columns={columns}
@@ -153,13 +182,10 @@ const Rating = () => {
 }
 
 export default Rating
-
 const StyledContainer = styled.main`
    padding: 100px 75px;
-   background: #217c3d;
-   font-family: 'Roboto Condensed', sans-serif !important;
-
-   /* background-color: #f0f3f7; */
+   background: linear-gradient(to bottom, #23a356, #18191b);
+   font-family: 'Roboto Condensed', serif;
 
    @media (max-width: 1024px) {
       padding: 100px 20px;
@@ -170,8 +196,6 @@ const StyledContainer = styled.main`
    }
 
    .table-container {
-      /* background-color: #fff; */
-      background: #217c3d;
       padding: 40px 20px;
       max-width: 1600px;
       margin: 0 auto;
@@ -189,10 +213,15 @@ const StyledContainer = styled.main`
       }
 
       .ant-table-thead > tr > th {
-         background: #fff;
-         border-bottom: 1.1px solid #dadde9;
+         background: transparent;
          font-size: 16px;
+         margin: 10px 15px;
+         color: white;
+         border: none;
 
+         &::before {
+            width: 0 !important;
+         }
          @media (max-width: 768px) {
             font-size: 12px;
          }
@@ -200,25 +229,47 @@ const StyledContainer = styled.main`
 
       .ant-table-tbody,
       .ant-table {
-         background-color: #217c3d !important;
+         background-color: transparent !important;
       }
+
       .ant-table-tbody > tr {
          padding: 10px;
       }
+
       .ant-table-tbody > tr > td {
          border: none;
-         background-color: white;
-         border-top: 1px solid #dadde9;
          font-size: 14px;
          margin: 10px;
          padding: 0;
-         border-radius: 10px;
+         transition: background-color 0.3s ease;
 
          @media (max-width: 768px) {
             font-size: 12px;
             max-width: 100%;
             padding: 10px;
          }
+      }
+
+      .ant-table-tbody > tr .text-content {
+         background: white;
+         padding: 10px 25px;
+         font-weight: 500;
+         transition: background-color 0.3s ease;
+      }
+
+      .table-container .ant-table-tbody > tr:hover {
+         background-color: transparent !important;
+         border-radius: 6px !important;
+      }
+
+      .ant-table-cell-row-hover {
+         background-color: transparent !important;
+         border-radius: 6px !important;
+      }
+      .ant-table-tbody > tr:hover .text-content {
+         background: #00a851;
+         color: white !important;
+         background-color: #e0e0e0 !important;
       }
    }
 `
