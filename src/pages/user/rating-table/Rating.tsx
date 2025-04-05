bdb533025beb8314ab994ca581c2bb1d769646e8
Bermet Sheralieva <bermetsheralieva@MacBook-Pro-Bermet.local>
2025-04-05 19:23:00 +0600

    save

--- src/pages/user/rating-table/Rating.tsx
+++ src/pages/user/rating-table/Rating.tsx
@@ -171,9 +171,11 @@ const Rating = () => {
       <StyledContainer>
          <Flex className="partners" align="center" justify="center">
             {partners?.slice(0, 3).map((item) => (
-               <div className="partner" key={item.id}>
-                  <img src={item.image} alt={item.title} width={70} />
-               </div>
+               <a href={item.link}>
+                  <div className="partner" key={item.id}>
+                     <img src={item.image} alt={item.title} width={70} />
+                  </div>
+               </a>
             ))}
          </Flex>
          <br />
